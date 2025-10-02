67c7064e6b6f7b609d1f4aa93245d1af156d9c15
Hyewon Yoon <iey704@gachon.ac.kr>
2025-10-02 13:57:39 +0900

    chore: 상수 메세지 분리

---
+++ src/constants/Messages.js
@@ -0,0 +1,13 @@
+export const MESSAGES = {
+  INPUT_CAR_NAMES:
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+  INPUT_MOVE_COUNT: "시도할 횟수를 입력하세요.\n",
+  RESULT_TITLE: "\n실행 결과:",
+};
+
+export const ERROR_MESSAGES = {
+  INVALID_CAR_NAME: "자동차 이름은 5자 이하만 가능합니다.",
+  EMPTY_CAR_NAME: "자동차 이름은 비어있을 수 없습니다.",
+  DUPLICATE_CAR_NAME: "자동차 이름은 중복될 수 없습니다.",
+  INVALID_MOVE_COUNT: "시도 횟수는 양의 정수여야 합니다.",
+};
